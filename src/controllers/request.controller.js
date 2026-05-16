5d67e85339eff9c46ee26784c93be8a5761224b2
AbhishekBisht23 <bishtabhishek2006@gmail.com>
2026-05-16 19:44:21 +0530

    add swapAccept and swapReject contorller

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -43,6 +43,66 @@ const createSwapRequest = asyncHandler(async (req , res)=>{
 
 })
 
+const acceptSwap = asyncHandler(async(req,res)=>{
+    const {swapId} = req.params
+    const swap = await Request.findById(swapId)
+    if(!swap)
+    {
+        throw new ApiError(404,"swap request not found")
+    }
+
+    if(swap.requestedItem.owner.toString() !== req.user._id.toString())
+    {
+        throw new ApiError(400,"unauthorized to accept request")
+    }
+    if(swap.requestStatus !== "pending")
+    {
+        throw new ApiError(400,"swap is already processed")
+    }
+
+    swap.requestStatus = "accepted"
+
+    swap.requestedItem.itemStatus = "swapped"
+    swap.offeredItem.itemStatus = "swapped"
+
+    await swap.save()
+    await swap.requestedItem.save()
+    await swap.offeredItem.save()
+
+    res.status(200).json(
+        new ApiResponse(200,swap,"swap request accepted succesfully")
+    )
+})
+
+const rejectSwap = asyncHandler(async(req,res)=>{
+    const {swapId} = req.params
+    const swap = await Request.findById(swapId)
+
+    if(!swap)
+    {
+        throw new ApiError(404,"swap request not found")
+    }
+
+    if(swap.requestedBy.toString() !== req.user._id)
+    {
+        throw new ApiError(400, "unauthorized")
+    }
+
+    if(swap.requestStatus !== "pending")
+    {
+        throw new ApiError(400, "swap request already processed")
+    }
+
+    swap.requestStatus = "rejected"
+
+    await swap.save()
+
+    res.status(200).json(
+        new ApiResponse(200,swap,"swap request rejected succefully")
+    )
+})
 export {
-    createSwapRequest
+    createSwapRequest,
+    acceptSwap,
+    rejectSwap
 }
